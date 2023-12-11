3433f24f13268acff0df9f5f5aa98ebe40b8a435
buttaRahul <butta.rahul.aditya@gmail.com>
2023-12-11 17:50:55 +0530

    corrected title field

--- tasks/models.py
+++ tasks/models.py
@@ -8,7 +8,7 @@ class Task(models.Model):
         2 : "In Progress",
         3 : "Completed",
     }
-    title = models.CharField(max_length=40)
+    title = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     due_date = models.DateTimeField()
     status = models.CharField(max_length=1,choices=STATUS_CHOICES)  
