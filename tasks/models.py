850c40b20053dc133e9a2416ac1a7c36c40a22de
buttaRahul <butta.rahul.aditya@gmail.com>
2023-12-11 18:00:12 +0530

    add Owner attribute(relation between task and user)

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -12,7 +13,6 @@ class Task(models.Model):
     description = models.CharField(max_length=200)
     due_date = models.DateTimeField()
     status = models.CharField(max_length=1,choices=STATUS_CHOICES)  
-    
-
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
 
     
