c54cdaecb3a15c34b3cb1a6b02fd207f60137f35
buttaRahul <butta.rahul.aditya@gmail.com>
2023-12-11 20:55:57 +0530

    modified IsOwner Permission

--- tasks/views.py
+++ tasks/views.py
@@ -8,8 +8,10 @@ from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        
         return request.user == obj.owner
+    
+    def has_permission(self, request, view):
+        return request.user.is_authenticated 
     
 class TaksViewset(ModelViewSet):
     permission_classes = [IsOwner]
