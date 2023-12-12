f8f5f1b9d31a437c6e443b1326fd60bdc7dabc31
buttaRahul <butta.rahul.aditya@gmail.com>
2023-12-12 14:08:23 +0530

    Fixed Bugs

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework.serializers import ModelSerializer
 from tasks.models import Task
-
-
+from datetime import datetime
+from rest_framework import serializers
 class TaskSerializer(ModelSerializer):
 
     class Meta:
         fields = "__all__"
         model = Task
 
-        
+    # def validate_due_date(self, value):
+    #     current_datetime = datetime.now()
+    #     print("VALUE",value)
+    #     print("CURRENT",current_datetime)
+    #     # if value <= current_datetime:
+    #     #     raise serializers.ValidationError("Due date must be greater than the current date and time.")
+
+    #     return value
+
--- tasks/tests.py
+++ tasks/tests.py
@@ -15,208 +15,215 @@ from rest_framework import status
 
 num_words = ["","ONE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NINE","TEN"]
 
-# class GetAllTasksTests(TestCase):
+class GetAllTasksTests(TestCase):
 
-#     client = APIClient()
+    client = APIClient()
 
-#     def setUp(self):
-#         pswd = make_password("abc123")
-#         user_schema = [
-#             {
-#                 "username": "user1",
-#                 "password" : pswd,
-#             },
-#             {
-#                 "username": "user2",
-#                 "password": pswd,
-#             }
+    def setUp(self):
+        pswd = make_password("abc123")
+        user_schema = [
+            {
+                "username": "user1",
+                "password" : pswd,
+            },
+            {
+                "username": "user2",
+                "password": pswd,
+            }
 
-#         ]
+        ]
 
-#         for user_data in user_schema:
-#            u =  User.objects.create(**user_data)
-#            Token.objects.create(user = u)
-
-
-#         users = User.objects.all()
-#         for i in range(1,11):
-#             rand_day = randint(1,4)
-#             dd = datetime.now() + timedelta(rand_day)
-#             task_data = {
-#                 "title":f"task{i}",
-#                 "description":f"This is task {num_words[i]}",
-#                 "due_date": dd,
-#                 "status" : 1,
-#                 "owner": users[randint(0,1)]
-#                 }
-#             Task.objects.create(**task_data)
-
-#         self.token = Token.objects.first()
+        for user_data in user_schema:
+           u =  User.objects.create(**user_data)
+           Token.objects.create(user = u)
 
 
-#     def test_get_all_tasks(self):
-#         url = "http://127.0.0.1:8000/tasks/"
-#         # self.tokens
-#         headers = {'HTTP_AUTHORIZATION': f'Token {self.token.key}'}
-#         response = self.client.get(url,**headers)
-#         tasks = Task.objects.all()
-#         seraializer = TaskSerializer(tasks,many = True)
+        users = User.objects.all()
+        for i in range(1,11):
+            rand_day = randint(1,4)
+            dd = datetime.now() + timedelta(rand_day)
+            task_data = {
+                "title":f"task{i}",
+                "description":f"This is task {num_words[i]}",
+                "due_date": dd,
+                "status" : 1,
+                "owner": users[randint(0,1)]
+                }
+            Task.objects.create(**task_data)
 
-#         self.assertEqual(response.data,seraializer.data)
-#         self.assertEqual(response.status_code,status.HTTP_200_OK)
-
-# class CreateNewTaskTests(TestCase):
-#     client = APIClient()
-
-
-#     def setUp(self):
-#         pswd = make_password("abc123")
-#         user_schema = [
-#             {
-#                 "username": "user1",
-#                 "password" : pswd,
-#             },
-#             {
-#                 "username": "user2",
-#                 "password": pswd,
-#             }
-
-#         ]
-
-#         for user_data in user_schema:
-#            u =  User.objects.create(**user_data)
-#            Token.objects.create(user = u)
-
-#         self.token = Token.objects.all()[0]
+        self.token = Token.objects.first()
 
 
-#     def test_post_valid_task(self):
-#         url = "http://127.0.0.1:8000/tasks/"
-#         data = {
-#             "title" : "task99",
-#             "description": "This is task 99",
-#             "due_date" : "2023-12-13",
-#             "status": 1,
-#             "owner" : User.objects.first().id,
-#         }
-#         headers = {'HTTP_AUTHORIZATION': f'Token {self.token.key}'}
-#         response = self.client.post(url,data=data,format = 'json',**headers)
+    def test_get_all_tasks(self):
+        print("TEST CASE FOR LIST ACTION")
+        url = "http://127.0.0.1:8000/tasks/"
+        # self.tokens
+        headers = {'HTTP_AUTHORIZATION': f'Token {self.token.key}'}
+        response = self.client.get(url,**headers)
+        tasks = Task.objects.all()
+        seraializer = TaskSerializer(tasks,many = True)
 
-#         self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+        self.assertEqual(response.data,seraializer.data)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+class CreateNewTaskTests(TestCase):
+    client = APIClient()
 
 
+    def setUp(self):
+        pswd = make_password("abc123")
+        user_schema = [
+            {
+                "username": "user1",
+                "password" : pswd,
+            },
+            {
+                "username": "user2",
+                "password": pswd,
+            }
 
-# class DeleteTaskTest(TestCase):
+        ]
 
-#     client = APIClient()
+        for user_data in user_schema:
+           u =  User.objects.create(**user_data)
+           Token.objects.create(user = u)
 
-#     def setUp(self):
-#         pswd = make_password("abc123")
-#         user_schema = [
-#             {
-#                 "username": "user1",
-#                 "password" : pswd,
-#             },
-#             {
-#                 "username": "user2",
-#                 "password": pswd,
-#             }
-#         ]
+        self.token = Token.objects.all()[0]
 
-#         for user_data in user_schema:
-#             u = User.objects.create(**user_data)
-#             Token.objects.create(user=u)
 
-#         self.token = Token.objects.all()[0]
+    def test_post_valid_task(self):
+        print("TEST CASE FOR POST ACTION")
 
-#         self.task_content = {
-#             "title": "task100",
-#             "description": "This is task hundred",
-#             "due_date": "2023-12-13",
-#             "status": 1,
-#             "owner": User.objects.first()
-#         }
-#         self.task = Task.objects.create(**self.task_content)
-
-#     def test_delete_task(self):
-#         url = f"http://127.0.0.1:8000/tasks/{self.task.id}/"
-#         headers = {'HTTP_AUTHORIZATION': f'Token {self.token.key}'}
-#         response = self.client.delete(url, **headers)
-#         print("IN TEST DELETE TASK")
-#         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        url = "http://127.0.0.1:8000/tasks/"
+        data = {
+            "title" : "task99",
+            "description": "This is task 99",
+            "due_date" : "2023-12-13",
+            "status": 1,
+            "owner" : User.objects.first().id,
+        }
+        headers = {'HTTP_AUTHORIZATION': f'Token {self.token.key}'}
+        response = self.client.post(url,data=data,format = 'json',**headers)
+        self.assertEqual(response.data['title'],data['title'])
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
 
 
 
-# class RetrieveSingleTaskTest(TestCase):
+class DeleteTaskTest(TestCase):
 
-#     client = APIClient()
+    client = APIClient()
 
-#     def setUp(self):
-#         pswd = make_password("abc123")
-#         user_schema = [
-#             {
-#                 "username": "user1",
-#                 "password" : pswd,
-#             },
-#             {
-#                 "username": "user2",
-#                 "password": pswd,
-#             }
-#         ]
+    def setUp(self):
+        pswd = make_password("abc123")
+        user_schema = [
+            {
+                "username": "user1",
+                "password" : pswd,
+            },
+            {
+                "username": "user2",
+                "password": pswd,
+            }
+        ]
 
-#         for user_data in user_schema:
-#             u = User.objects.create(**user_data)
-#             Token.objects.create(user=u)
+        for user_data in user_schema:
+            u = User.objects.create(**user_data)
+            Token.objects.create(user=u)
 
-#         self.token = Token.objects.all()[0]
+        self.token = Token.objects.all()[0]
 
-#         self.task_content = {
-#             "title": "task100",
-#             "description": "This is task hundred",
-#             "due_date": "2023-12-13",
-#             "status": 1,
-#             "owner": User.objects.first()
-#         }
-#         self.task = Task.objects.create(**self.task_content)
+        self.task_content = {
+            "title": "task100",
+            "description": "This is task hundred",
+            "due_date": "2023-12-13",
+            "status": 1,
+            "owner": User.objects.first()
+        }
+        self.task = Task.objects.create(**self.task_content)
 
-#     def test_retrieve_task(self):
-#         url = f"http://127.0.0.1:8000/tasks/{self.task.id}/"
-#         headers = {'HTTP_AUTHORIZATION': f'Token {self.token.key}'}
-#         response = self.client.get(url, **headers)
-#         print("IN RETREIVE SINGLE TASK")
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_delete_task(self):
+        print("TEST CASE FOR DELETE ACTION")
+        url = f"http://127.0.0.1:8000/tasks/{self.task.id}/"
+        headers = {'HTTP_AUTHORIZATION': f'Token {self.token.key}'}
+        response = self.client.delete(url, **headers)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
 
-# class UpdateTaskTest(APITestCase):
-#     client = APIClient()
 
-#     def setUp(self):
-#         pswd = make_password("abc@123")
+class RetrieveSingleTaskTest(TestCase):
 
-#         self.user = User.objects.create(username = "user1",password = pswd)
+    client = APIClient()
+
+    def setUp(self):
+        pswd = make_password("abc123")
+        user_schema = [
+            {
+                "username": "user1",
+                "password" : pswd,
+            },
+            {
+                "username": "user2",
+                "password": pswd,
+            }
+        ]
+
+        for user_data in user_schema:
+            u = User.objects.create(**user_data)
+            Token.objects.create(user=u)
+
+        self.token = Token.objects.all()[0]
+
+        self.task_content = {
+            "title": "task100",
+            "description": "This is task hundred",
+            "due_date": "2023-12-13",
+            "status": 1,
+            "owner": User.objects.first()
+        }
+        self.task = Task.objects.create(**self.task_content)
+
+    def test_retrieve_task(self):
+        print("TEST CASE FOR SINGLE TASK RETRIEVAL")
+        url = f"http://127.0.0.1:8000/tasks/{self.task.id}/"
+        headers = {'HTTP_AUTHORIZATION': f'Token {self.token.key}'}
+        response = self.client.get(url, **headers)
+        self.assertEqual(response.data['title'],self.task_content['title'])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class UpdateTaskTest(APITestCase):
+    client = APIClient()
+
+    def setUp(self):
+        pswd = make_password("abc@123")
+
+        self.user = User.objects.create(username = "user1",password = pswd)
         
-#         self.token = Token.objects.create(user = self.user)
+        self.token = Token.objects.create(user = self.user)
 
-#         self.task = Task.objects.create(title = "task100",description = "This is task Hundred", due_date = "2023-12-13",status = 1, owner = self.user)
+        self.task = Task.objects.create(title = "task100",description = "This is task Hundred", due_date = "2023-12-13",status = 1, owner = self.user)
 
     
-#     def test_put_task(self):
+    def test_put_task(self):
+        print("TEST CASE OF PUT ACTION")
 
-#         test_data = {
-#             "title" : "task101",
-#             "description": "This is task Hundred",
-#             "due_date": "2023-12-13",
-#             "status": 1,
-#             "owner" : self.user.id,
+        test_data = {
+            "title" : "task101",
+            "description": "This is task Hundred",
+            "due_date": "2023-12-13",
+            "status": 1,
+            "owner" : self.user.id,
 
-#         }
-
-#         url = f"http://127.0.0.1:8000/tasks/{self.task.id}/"
-#         print(url)
-#         headers = {'HTTP_AUTHORIZATION': f'Token {self.token.key}'} 
-#         response = self.client.put(url,data=test_data,**headers)
-#         print("IN PUT TASK")
-#         self.assertEqual(response.status_code,status.HTTP_200_OK)
+        }
+        
+        url = f"http://127.0.0.1:8000/tasks/{self.task.id}/"
+        print(url)
+        headers = {'HTTP_AUTHORIZATION': f'Token {self.token.key}'} 
+        response = self.client.put(url,data=test_data,**headers)
+        print("IN PUT TASK")
+        self.assertNotEqual(response.data['title'],self.task.title)
+        self.assertEqual(response.data['title'],test_data['title'])
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
 
 
 
@@ -232,7 +239,8 @@ class UpdateTaskOtherThanOwner(APITestCase):
 
         self.task = Task.objects.create(title = "task100",description = "This is task Hundred", due_date = "2023-12-13",status = 1, owner = self.user1)
         
-    def test_retrieve_than_owner(self):
+    def test_retrieve_other_than_owner(self):
+        print("TEST CASE TO PREVENT CROSS ACCESS ")
         url = f"http://127.0.0.1:8000/tasks/{self.task.id}/"
         headers = {'HTTP_AUTHORIZATION': f'Token {self.token2.key}'}
         response = self.client.get(url,**headers)
@@ -247,7 +255,6 @@ class UpdateTaskOtherThanOwner(APITestCase):
 
       
 
-# python manage.py test -v 2
 
 
 
